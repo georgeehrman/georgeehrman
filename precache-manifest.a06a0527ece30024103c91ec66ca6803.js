96b1ce2c7d9d222ce00cfef1e7aa3352751169b2
george ehrman <georgeehrman@georges-MacBook-Air.local>
2019-12-23 23:08:52 -0800

    Updates

---
+++ precache-manifest.a06a0527ece30024103c91ec66ca6803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c346c78b67e44f77171fb9790f7826",
+    "url": "/georgeehrman/index.html"
+  },
+  {
+    "revision": "8c5657e82cc2069c3a0a",
+    "url": "/georgeehrman/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "15ccd74a846026f2643c",
+    "url": "/georgeehrman/static/js/2.fcbb71a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/georgeehrman/static/js/2.fcbb71a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c5657e82cc2069c3a0a",
+    "url": "/georgeehrman/static/js/main.33f6e1c4.chunk.js"
+  },
+  {
+    "revision": "7ff6939d8f434057d5b3",
+    "url": "/georgeehrman/static/js/runtime-main.1135ee21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/georgeehrman/static/media/logo.5d5d9eef.svg"
+  }
+]);
